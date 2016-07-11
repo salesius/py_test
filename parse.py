4245f492d31fe7d233bec090c3d20287db7fe000
fsal <salesius7@naver.com>
2016-07-11 11:06:26 -0700

    test2

--- parse.py
+++ parse.py
@@ -3,8 +3,9 @@
 
 import sys
 import string
+import json
 
-section2 = [
+section = [
 	"<id>",
 	"<title>",
 	"<author>",
@@ -37,84 +38,56 @@ section2 = [
 	"<notes>"
 ] 
 
-section = [ "<id>",
-	"<title>",
-	"<author>",
-	"<pages>",
-	"<abstract>",
-	"<eauthor>",
-	"<eabstract>",
-	"<classification>",
-	"<keywords>",
-	"<notes>"
-	""
-]
 
-#f = open("one.kt", "r")
+listInt = [0,4,5,6,7,8,9,10,11,12,15,19,20,21]
 
-#line = f.readline()
-#while True:
-
-w = open("result.json", "w")
 w2 = open("result2.json", "w")
 s_num = 0
 l_count = 0
 
-out=["","","","","","","","","","",""]
+out=[None]*len(section)
 
-with open("one.kt", "r") as f :
+
+#with open("one_16", "r") as f :
+with open("kt.doc.56", "r") as f :
 	for line in f :
 		s_check = line.find(section[s_num])
-		
-	
+
 		if s_check != -1 :
-			if s_num == 0 :
-				w.write("{")
-	
-			if s_num != 0 :
-				w.write(",")
-
 			temp = section[s_num]
-			w.write("\"")
-			w.write(temp[1:len(temp)-1]+"\""+":")
-
-			print "num : ", s_num
-			print line[len(temp):]
-#content = line[len(temp):].strip("\n").strip("\t")
 			content = line[len(temp):].translate(None, "\t\n")
-			print "content : ", content
+
 			if s_num == 0 :
-				w.write(str(int(content)))
 				out[s_num] = str(int(content))
 			else :
-				w.write(content)
 				out[s_num] = content
-#w.write(line[len(temp):].strip("\n"))
-			print line;
+
 			s_num = s_num+1
+
 		else :
-			print line + "========="
-			w.write(line.strip("\n"))
-			# index값을 -1 줘야하는것을 잏지 말자
-#out[s_num-1] = out[s_num-1] + line.strip("\n").strip("\t")
 			out[s_num-1] = out[s_num-1] + line.translate(None, "\t\n")
+
+
 		if s_num == len(section): 
 			s_num = 0
-			w.write("}\n")
+			w2.write("{\"index\":{\"_id\":\"")
+			w2.write(out[0])
+			w2.write("\"}}\n")
 
-			#write file 2
 			w2.write("{")
 			num=0
 			for tt in out :
 				w2.write("\"")	
 				temp2 = section[num]
 				w2.write(temp2[1:len(temp2)-1]+"\""+":")
-				if num == 0 :
+
+				#integer인 센션들은 이렇게 처리하기 " 필요없으므로
+				if num in listInt :
 					w2.write(tt)
 					w2.write(",")
 				else :
 					w2.write("\"")	
-					w2.write(tt)
+					w2.write(tt.replace('"', '\\"'))
 					w2.write("\"")	
 					if num != len(section)-1 :
 						w2.write(",")
@@ -125,7 +98,6 @@ with open("one.kt", "r") as f :
 					break
 				
 			w2.write("}\n")
-			#end file 2
-	
-		l_count = l_count + 1
 
+
+		l_count = l_count + 1
